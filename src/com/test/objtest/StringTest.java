16b0808cac24a35d7fa1464551e978e722796e4d
gongjianfeng1 <gongjianfeng1@bngrp.com>
2021-03-13 13:18:16 +0800

    提交所有的改动

--- src/com/test/objtest/StringTest.java
+++ src/com/test/objtest/StringTest.java
@@ -1,5 +1,9 @@
 package com.test.objtest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 /**
  * @Description:
  * @Copyright: Copyright (c) 2020  ALL RIGHTS RESERVED.
@@ -14,6 +18,20 @@ package com.test.objtest;
 public class StringTest {
 
     public static void main(String[] args) {
+        loodTest();
+    }
+
+    private static void loodTest() {
+
+        long time1 = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            System.out.println(i);
+        }
+        long time2 = System.currentTimeMillis();
+        System.out.println((time2-time1));
+    }
+
+    private static void testBi() {
         String s = String.format("asd:%s, sadasa:%s","2","3");
         System.out.println(s);
     }
